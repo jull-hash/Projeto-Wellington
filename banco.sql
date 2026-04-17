691885b9d6d3727a2f9aed92618b94285e1283ca
jull-hash <dacruznetojulio@gmail.com>
2026-04-16 21:15:35 -0300

    Create banco.sql

---
+++ banco.sql
@@ -0,0 +1,51 @@
+create database papelaria;
+use papelaria;
+
+create table usuario (
+id_usuario int auto_increment primary key,
+imagem_usuario varchar(255),
+nome_usuario varchar(100) not null,
+endereco varchar(255),
+email varchar(255) not null,
+telefone varchar(12),
+user_criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+tipo enum('user','adm') not null default 'aluno'
+);
+
+create table livro (
+id_livro int auto_increment primary key,
+nome_livro varchar(100) not null,
+autor varchar(100),
+publicado varchar(100),
+genero varchar(100),
+preco_livro decimal(10,2) not null,
+imagem_livro varchar(255),
+descricao_livro text,
+quantidade_livro int default (0),
+prod_criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+create table produto (
+id_produto int auto_increment primary key,
+nome_produto varchar(100) not null,
+preco_produto decimal(10,2) not null,
+imagem_produto varchar(255),
+quantidade_produto int default (0),
+prod_criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+create table estoque (
+    id_estoque int auto_increment primary key,
+    nome_estoque varchar(100) not null,
+    
+    
+    produto_id INT, 
+    CONSTRAINT fk_produto_guardar 
+    FOREIGN KEY (produto_id) 
+    REFERENCES produto(id_produto),
+    
+    livro_id int,
+    constraint fk_livro_guardar
+    foreign key (livro_id)
+    references livro(id_livro)
+    );
