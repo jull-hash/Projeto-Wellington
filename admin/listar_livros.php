4c418d44afad497dd75b9a79e4416085a4b6a50a
My Name <my@example.com>
2026-04-27 22:01:06 -0300

    fazendo o edit e delete

--- admin/listar_livros.php
+++ admin/listar_livros.php
@@ -5,7 +5,7 @@ session_start();
 
 
 // Incluir o arquivo de conexão com o banco
-require_once "conexao.php";
+require_once "../conexao.php";
 
 // Variáveis para mensagens
 $sucesso = "";
@@ -15,7 +15,7 @@ $editando= null;
 
 
 if (isset($_GET["editar"])){
-    $id = $_GET["editar"];
+    $id_livro = $_GET["editar"];
     $sql = "SELECT * FROM livro WHERE id_livro = $id_livro";
     $res=mysqli_query($conexao,$sql);
     $editando = mysqli_fetch_assoc($res);
@@ -25,12 +25,12 @@ if (isset($_GET["editar"])){
 
   
 // Verificar se o email já existe
-$sql = "SELECT * FROM livro WHERE nome_livro = '$nome_livro'";
+$sql = "SELECT * FROM livro WHERE id_livro = '$id_livro'";
 $resultado = mysqli_query($conexao, $sql);
 
 if (empty($error)){
   if (!empty($_POST["id_livro"])) {
-      $id = $_POST["id_livro"];
+      $id_livro = $_POST["id_livro"];
       $sqlcurform = "UPDATE livro
               SET nome_livro='$nome_livro',
               autor='$autor',
@@ -40,13 +40,13 @@ if (empty($error)){
               imagem_livro='$strimagem_livro',
               descricao_livro='$descricao_livro',
               quantidade_livro='$quantidade_livro',
-              WHERE id_livro = '$id'";
+              WHERE id_livro = '$id_livro'";
   }
 }
 
 // Buscar todos os livros para listar
-$sql = "SELECT imagem_livro, nome_livro, autor, editora, quantidade_livro FROM livro ORDER BY id DESC";
-$usuarios = mysqli_query($conexao, $sql);
+$sql = "SELECT imagem_livro, nome_livro, autor, genero, quantidade_livro FROM livro ORDER BY id_livro  DESC";
+$livros = mysqli_query($conexao, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -64,14 +64,14 @@ $usuarios = mysqli_query($conexao, $sql);
       <h2>Painel<br>Admin</h2>
     </div>
     <nav class="menu-nav">
-      <a href="dashboard.html">Dashboard</a>
-      <a href="pedidos.html">Pedidos / Vendas</a>
+      <a href="dashboard.php">Dashboard</a>
+      <a href="vendas.php">Pedidos / Vendas</a>
       <p style="font-size: 0.8rem; font-weight: 700; color: var(--marrom-claro); margin: 1rem 0 0.2rem 1rem; text-transform: uppercase;">Catálogo</p>
-      <a href="listar_livros.html" class="ativo">Lista de Livros</a>
-      <a href="cadastro_livro.html">Adicionar Livro</a>
+      <a href="listar_livros.php" class="ativo">Lista de Livros</a>
+      <a href="cadastro_libro.php">Adicionar Livro</a>
       <p style="font-size: 0.8rem; font-weight: 700; color: var(--marrom-claro); margin: 1rem 0 0.2rem 1rem; text-transform: uppercase;">Sistema</p>
-      <a href="usuarios.html">Gerenciar Usuários</a>
-      <a href="../login.html" style="color: #c62828; margin-top: 1rem;">Sair</a>
+      <a href="gerenciar_usuarios.php">Gerenciar Usuários</a>
+      <a href="../login.php " style="color: #c62828; margin-top: 1rem;">Sair</a>
     </nav>
   </aside>
 
@@ -88,24 +88,41 @@ $usuarios = mysqli_query($conexao, $sql);
             <th>Título</th>
             <th>Autor</th>
             <th>Editora</th>
-            <th>Estoque</th>
+            <th>Gênero</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><div class="capa-miniatura" style="background-color: #4A769E;"></div></td>
-            <td style="font-weight: 700;">O Senhor dos Anéis</td>
-            <td>J.R.R. Tolkien</td>
-            <td>HarperCollins</td>
-            <td>9 un.</td>
-            <td>
-              <div class="acoes-tabela">
-                <button class="btn-acao">✏️</button>
-                <button class="btn-acao btn-excluir">🗑️</button>
-              </div>
-            </td>
+          <?php  while ($u = mysqli_fetch_assoc($livros)): ?>
+                        <tr class="border-b border-gray-200 hover:bg-gray-50">
+                            <td class="px-4 py-3"><?php echo $idVisual +=1; ?></td>
+                            <td class="px-4 py-3"><?php echo $u["nome_livro"]; ?></td>
+                            <td class="px-4 py-3">
+                            <?php if (!empty($u["imagem_livro"])): ?>
+                                <img src="uploads/<?= $u["imagem_livro"] ?>"
+                                    width="60">
+                            <?php else: ?>
+                                Sem imagem
+                            <?php endif; ?>
+                        </td>
+                            <td class="px-4 py-3 text-gray-500"><?php echo $u["criado_em"]; ?></td>
+                            <td class="px-4 py-3 text-gray-500"><?php echo $u["preco_livro"]; ?></td>
+                            <td class="px-4 py-3 ">
+                            <a href="delete.php?id=<?php echo $u['id_livro']; ?>&tabela=produtos&pagina=cadastro_libro.php" onclick="return confirm('Tem certeza?');" 
+                            class="flex items-center px-6 py-3 text-red-900 hover:text-red-700 tex t-lg">
+                                🗑️
+                                Excluir
+                                </a>
+                                <a href="editar.php?id_livro=<?= $u['id_livro']?>"
+                                class="flex items-center px-6 py-3 text-blue-600 hover:text-red-500">
+                                📝 
+                                Editar
+                            </a>
+                            </td>
+                            </td>
           </tr>
+          <?php endwhile; ?>
         </tbody>
       </table>
     </div>
